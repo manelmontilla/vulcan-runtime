fc8e8752a559c944295369f26430d9889e8b6f0a
Manel Montilla <manel.montilla@gmail.com>
2023-11-12 14:30:29 +0100

    WIP

--- internal/checktype/checktype.go
+++ internal/checktype/checktype.go
@@ -4,4 +4,5 @@ import (
 	catalog "github.com/adevinta/vulcan-check-catalog/pkg/model"
 )
 
+// Checktype defines the information of a Checktype.
 type Checktype catalog.Checktype
--- internal/checktype/image.go
+++ internal/checktype/image.go
@@ -2,19 +2,11 @@ package checktype
 
 import (
 	"fmt"
-	"time"
-
-	checkcatalog "github.com/adevinta/vulcan-check-catalog/pkg/model"
 
 	"github.com/manelmontilla/vulcan-runtime/internal/dockerutil"
 )
 
 const (
-	// lasModifiedTimeLabel defines the key of the label using [reverse DNS notation].
-	//
-	// [reverse DNS notation]:https://docs.docker.com/config/labels-custom-metadata/
-	lastModifiedTimeLabel = "com.adevinta.vulcan.last_modified_file"
-
 	// checktypeNameLabel defines the key of the label using [reverse DNS notation].
 	//
 	// [reverse DNS notation]:https://docs.docker.com/config/labels-custom-metadata/
@@ -26,7 +18,7 @@ const (
 	checktypeManifest = "com.adevinta.vulcan.manifest"
 )
 
-// ErrNoChecktypeImage is returned by the [ParseImage] function when an image
+// ErrNoChecktypeImage is returned by the [ImageFromRef] function when an image
 // does not contain the metadata of a checktype.
 type ErrNoChecktypeImage struct {
 	Image string
@@ -47,75 +39,60 @@ type Image struct {
 
 	// Manifest the manifest of the checktype that the image contains.
 	Manifest Manifest
-
-	// LastModified contains the time the code of the checktype stored in the
-	// image was modified.
-	LastModified time.Time
 }
 
-// InspectImage returns the metadata about a checktype stored in an image.
-func InspectImage(image string) (Image, error) {
+// ImageFromRef returns the information of a checktype stored in the image
+// pointed by a ref.
+func ImageFromRef(ref string) (Image, error) {
 	cli, err := dockerutil.NewAPIClient()
 	if err != nil {
 		return Image{}, fmt.Errorf("unable to instantiate a docker client: %v", err)
 	}
-	labels, err := dockerutil.ImageLabels(cli, image)
+	labels, err := dockerutil.ImageLabels(cli, ref)
 	if err != nil {
 		return Image{}, fmt.Errorf("unable to read image labels: %w", err)
-	}
-	lastModified, ok := labels[lastModifiedTimeLabel]
-	if !ok {
-		err := ErrNoChecktypeImage{Image: image}
-		return Image{}, fmt.Errorf("%w: label %s not found", err, lastModifiedTimeLabel)
-	}
-	lastModifiedTime, err := time.Parse(time.RFC822, lastModified)
-	if err != nil {
-		errNoCheck := ErrNoChecktypeImage{Image: image}
-		err := fmt.Errorf("invalid time %s defined in the label %s: %w", lastModified, lastModifiedTimeLabel, errNoCheck)
-		return Image{}, err
 	}
 
 	ctName, ok := labels[checktypeNameLabel]
 	if !ok {
-		err := ErrNoChecktypeImage{Image: image}
+		err := ErrNoChecktypeImage{Image: ref}
 		return Image{}, fmt.Errorf("label %s not found: %w", checktypeNameLabel, err)
 	}
 
 	m, ok := labels[checktypeManifest]
 	if !ok {
-		err := ErrNoChecktypeImage{Image: image}
+		err := ErrNoChecktypeImage{Image: ref}
 		return Image{}, fmt.Errorf("label %s not found: %w", checktypeManifest, err)
 	}
 
 	manifest, err := ParseManifest(m)
 	if err != nil {
-		err := ErrNoChecktypeImage{Image: image}
+		err := ErrNoChecktypeImage{Image: ref}
 		return Image{}, fmt.Errorf("invalid checktype manifest: %w", err)
 	}
 
 	return Image{
-		Name:          image,
+		Name:          ref,
 		ChecktypeName: ctName,
 		Manifest:      manifest,
-		LastModified:  lastModifiedTime,
 	}, nil
 }
 
 // Checktype returns the information of the checktype defined in the image.
-func (i Image) Checktype() (checkcatalog.Checktype, error) {
+func (i Image) Checktype() (Checktype, error) {
 	options, err := i.Manifest.UnmarshalOptions()
 	if err != nil {
-		return checkcatalog.Checktype{}, fmt.Errorf("unable to unmarshal options: %w", err)
+		return Checktype{}, fmt.Errorf("unable to unmarshal options: %w", err)
 	}
 	assetTypes, err := i.Manifest.AssetTypes.Strings()
 	if err != nil {
-		return checkcatalog.Checktype{}, fmt.Errorf("unable to read asset types: %w", err)
+		return Checktype{}, fmt.Errorf("unable to read asset types: %w", err)
 	}
 	var requiredVars []any
 	for _, r := range i.Manifest.RequiredVars {
 		requiredVars = append(requiredVars, r)
 	}
-	ct := checkcatalog.Checktype{
+	ct := Checktype{
 		Name:         i.ChecktypeName,
 		Description:  i.Manifest.Description,
 		Image:        i.Name,
